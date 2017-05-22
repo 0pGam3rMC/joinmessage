b92f05f9b553522872efbf28fe5163bfa4cd3bab
0pGam3rMC <kuasir321@gmail.com>
2017-05-22 15:08:49 -0700

    Update OPjoin.php

--- src/0pGam3rMC/OPjoin/OPjoin.php
+++ src/0pGam3rMC/OPjoin/OPjoin.php
@@ -11,7 +11,12 @@ use pocketmine\command\ComandSender;
 class OPjoin extends PluginBase implements Listener(
 public function onLoad(){
    $this->getLogger()->info("OPjoin.1.0.0 is being loaded")
-    $this->getServer()->broadcastMessage(TextFormat::AQUA $name has joined the craziness");
+     $this->getServer()->broadcastMessage(TextFormat::AQUA "$name has joined the craziness");
+}
 public function on Command (CommandSender $sender,Command $cmd, $label, array $args){
-
+if($cmd->getName("test"){
+  $sender->getInventory()->addItem(Item:get(276,0,5);
+  $sender->sendMessage(TextFormat::AQUA "YOU HAVE RECIEVED 5 DIAMOND SWORDS");
+  }
+  
 
