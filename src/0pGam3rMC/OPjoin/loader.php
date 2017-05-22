e041ed1dab84ff57faa9105b69e5587f55095abc
0pGam3rMC <kuasir321@gmail.com>
2017-05-22 14:39:50 -0700

    Update loader.php

--- src/0pGam3rMC/OPjoin/loader.php
+++ src/0pGam3rMC/OPjoin/loader.php
@@ -1,1 +1,17 @@
+<?php
+namespace OPjoin
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\command\Command;
+use pocketmine\command\ComandSender;
+class OPjoin extends PluginBase implements Listener(
+public function onLoad(){
+   $this->getLogger()->info("OPjoin.1.0.0 is being loaded")
+    $this->getServer()->broadcastMessage(TextFormat::AQUA $name has joined the craziness");
+public function on Command (CommandSender $sender,Command $cmd, $label, array $args){
+
 
